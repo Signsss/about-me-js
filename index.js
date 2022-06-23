012f68aadd0a984fdf91564477810346c020aa5e
Signsss <105465711+Signsss@users.noreply.github.com>
2022-06-23 14:27:05 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = 'Wisdom';
+const height = 6;
+const country = 'Nigeria';
+
+console.log(`My name is ${name}`);
+console.log(`I am ${height} feet tall`);
+console.log(`I live in ${country}`);
